9dcf7ac70f8d6eb1b99cbecbcf1b2a88f92fc98e
jlfarnes <jacob.farnes@gmail.com>
2016-05-24 14:36:15 -0600

    Commit control classes organized commit with data done 2016-05-24 14:36 MST

--- whereisblackbeard/src/byui/cit260/whereisblackbeard/control/Port/ArmoryControls.java
+++ whereisblackbeard/src/byui/cit260/whereisblackbeard/control/Port/ArmoryControls.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byui.cit260.whereisblackbeard.control;
+package byui.cit260.whereisblackbeard.control.Port;
 
 /**
  *
  * @author josephclark
  */
-public class MapControls {
+public class ArmoryControls {
     
 }
