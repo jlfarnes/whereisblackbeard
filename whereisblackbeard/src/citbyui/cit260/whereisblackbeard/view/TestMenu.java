5b5514f81c542402922a76bfce53b827f857899d
jlfarnes <jlfarnes@byui.edu>
2016-06-08 23:43:01 -0600

    commit with data done 2016-06-08 23:42 MST

--- whereisblackbeard/src/citbyui/cit260/whereisblackbeard/view/TestMenu.java
+++ whereisblackbeard/src/citbyui/cit260/whereisblackbeard/view/TestMenu.java
@@ -5,10 +5,91 @@
  */
 package citbyui.cit260.whereisblackbeard.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jlfarnes
  */
 public class TestMenu {
     
+    private String menus;
+    private String choice;
+    boolean done = false; // set flag to not done
+    
+    public void loadTestMenuText() {
+        this.menus = "\n"
+                + "\nTest Menu"
+                + "\n========="
+                + "\nS - Starting Cove View"
+                + "\nP - Port View"
+                + "\nC - Combat View"
+                + "\n"
+                + "\n============="
+                + "\nM - Main Menu";
+    }
+    
+    public void displayTestMenu() {
+        
+        
+        do {
+            // prompt for and get players name
+            String testOption = this.getTestOption();
+            if (testOption.toUpperCase().equals("M")) // user wants to quit
+                //return; // exit the game\
+                displayMainMenu();
+            
+            // do the requested action and display the next view
+            done = this.doAction(testOption);
+        } while (!done);
+    }
+    
+    public String getTestOption() {
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = ""; // value to be returned
+        boolean valid = false; // initialize to not valid
+        
+        while (!valid) { // loop while an invalid value is entered
+            System.out.println("\n" + this.choice);
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length()  < 1) {
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+            }
+            
+            break; // end of loop
+        }
+            
+        
+        return value; 
+    }
+    
+    public void doAction(testOption) {
+        //System.out.println("doing nothing for now");
+        
+        switch (choice) {
+            case "S":
+                System.out.println("\nStarting Cove View called");
+                //displayStartingCove();
+                break;
+            case "P":
+                System.out.println("\nPort View called");
+                //PortView.displayPortView();
+                break;
+            case "C":
+                System.out.println("\nCombat View called");
+                //displayCombatView();
+                break;
+            case "M":
+                System.out.println("\nMain Menu View called");
+                //displayMainMenu();
+                break;
+            default:
+                System.out.println("\nInvalid value");
+                break;
+        }
+    }
 }
