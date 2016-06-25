652b01f8be39174add5bccf9920e2585affaa2cd
jlfarnes <jlfarnes@byui.edu>
2016-06-24 22:02:10 -0600

    Hacking away at GameMenuView

--- whereisblackbeard/src/citbyui/cit260/whereisblackbeard/view/GameMenuView.java
+++ whereisblackbeard/src/citbyui/cit260/whereisblackbeard/view/GameMenuView.java
@@ -87,6 +87,7 @@ public class GameMenuView extends View {
                         + ("\n Row? : ");
         */    
         
+        /*
         //Display Title
         System.out.println("\n|--------- MAP ---------|");
         System.out.println("\n|-----------------------|");
@@ -109,6 +110,46 @@ public class GameMenuView extends View {
             System.out.print(" |");
         }
         System.out.print(" |");
+        */
+        
+        // public void displayMap() {
+            String leftIndicator;
+            String rightIndicator;
+
+            Game game = whereisblackbeard.getCurrentGame(); // retreive the game
+            Map map = game.getMap(); // retreive the map from game
+            Location[][] locations = map.getLocations(); // retreive the locations from map
+            try {
+              this.console.print(" |");
+              for( int column = 0; column < locations[0].length; column++){
+                this.console.print(" " + column + " |"); // print col numbers to side of map
+              }
+              this.console.println();
+              for( int row = 0; row < locations.length; row++){
+                this.console.print(row + " "); // print row numbers to side of map
+                for( int column = 0; column < locations[row].length; column++){
+                  leftIndicator = " ";
+                  rightIndicator = " ";
+                  if(locations[row][column] == map.getCurrentLocation()){
+                    leftIndicator = "*"; // can be stars or whatever these are indicators showing visited
+                    rightIndicator = "*"; // same as above
+                  }
+                  else if(locations[row][column].isVisited()){
+                     leftIndicator = ">"; // can be stars or whatever these are indicators showing visited
+                     rightIndicator = "<"; // same as above
+                  }
+                  this.console.print("|"); // start map with a |
+                  if(locations[row][column].getScene() == null)
+                    this.console.print(leftIndicator + "??" + rightIndicator);
+                  else
+                    this.console.print(leftIndicator + locations[row][column].getScene().getMapSymbol() + rightIndicator);
+                }
+                this.console.println("|");
+              }
+            }catch (Exception e) {
+              System.out.println("Error");
+            }
+          }
     }
     
     private void playerFood() {
