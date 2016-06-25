caec5c266962296edbd605ff2b1478072e6ce6ae
jlfarnes <jlfarnes@byui.edu>
2016-06-24 22:54:04 -0600

    Hacking away at Location constructor & GameMenuView

--- whereisblackbeard/src/citbyui/cit260/whereisblackbeard/view/GameMenuView.java
+++ whereisblackbeard/src/citbyui/cit260/whereisblackbeard/view/GameMenuView.java
@@ -5,8 +5,8 @@
  */
 package citbyui.cit260.whereisblackbeard.view;
 
-import java.util.Scanner;
-import byui.cit260.whereisblackbeard.model.Map;
+//import java.util.Scanner;
+//import byui.cit260.whereisblackbeard.model.Map;
 
 /**
  *
@@ -40,7 +40,7 @@ public class GameMenuView extends View {
         switch (choice) {
             case "M":
                 // Map map = new Map();
-                this.displayMap(); //map.getLocName(), map.getNoOfRows(), map.getNoOfColumns());
+                this.displayMap();
                 break;
             case "F": // create and start a new game
                 this.playerFood();
@@ -68,43 +68,43 @@ public class GameMenuView extends View {
         return false;
     }
     
-        public void displayMap() {
-            String leftIndicator;
-            String rightIndicator;
+    public void displayMap() {
+        String leftIndicator;
+        String rightIndicator;
 
-            Game game = game.getCurrentGame(); // retreive the game
-            Map map = game.getMap(); // retreive the map from game
-            Location[][] locations = map.getLocations(); // retreive the locations from map
-            try {
-              System.out.print(" |");
-              for( int column = 0; column < locations[0].length; column++){
-                System.out.print(" " + column + " |"); // print col numbers to side of map
+        Game game = game.getCurrentGame(); // retreive the game
+        Map map = game.getMap(); // retreive the map from game
+        Location[][] locations = map.getLocations(); // retreive the locations from map
+        try {
+          System.out.print(" |");
+          for( int column = 0; column < locations[0].length; column++){
+            System.out.print(" " + column + " |"); // print col numbers to side of map
+          }
+          System.out.println();
+          for( int row = 0; row < locations.length; row++){
+            System.out.print(row + " "); // print row numbers to side of map
+            for( int column = 0; column < locations[row].length; column++){
+              leftIndicator = " ";
+              rightIndicator = " ";
+              if(locations[row][column] == map.getCurrentLocation()){
+                leftIndicator = "*"; // can be stars or whatever these are indicators showing visited
+                rightIndicator = "*"; // same as above
               }
-              System.out.println();
-              for( int row = 0; row < locations.length; row++){
-                System.out.print(row + " "); // print row numbers to side of map
-                for( int column = 0; column < locations[row].length; column++){
-                  leftIndicator = " ";
-                  rightIndicator = " ";
-                  if(locations[row][column] == map.getCurrentLocation()){
-                    leftIndicator = "*"; // can be stars or whatever these are indicators showing visited
-                    rightIndicator = "*"; // same as above
-                  }
-                  else if(locations[row][column].isVisited()){
-                     leftIndicator = ">"; // can be stars or whatever these are indicators showing visited
-                     rightIndicator = "<"; // same as above
-                  }
-                  System.out.print("|"); // start map with a |
-                  if(locations[row][column].getScene() == null)
-                    System.out.print(leftIndicator + "??" + rightIndicator);
-                  else
-                    System.out.print(leftIndicator + locations[row][column].getScene().getMapSymbol() + rightIndicator);
-                }
-                this.console.println("|");
+              else if(locations[row][column].isVisited()){
+                 leftIndicator = ">"; // can be stars or whatever these are indicators showing visited
+                 rightIndicator = "<"; // same as above
               }
-            }catch (Exception e) {
-              System.out.println("Error");
+              System.out.print("|"); // start map with a |
+              if(locations[row][column].getScene() == null)
+                System.out.print(leftIndicator + "??" + rightIndicator);
+              else
+                System.out.print(leftIndicator + locations[row][column].getScene().getMapSymbol() + rightIndicator);
             }
+            System.out.println("|");
+          }
+        }catch (Exception e) {
+          System.out.println("Error");
+        }
           
     }
     
@@ -130,8 +130,6 @@ public class GameMenuView extends View {
     }
     
     private void displayTestMenu() {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
         TestMenu testMenu = new TestMenu();
         testMenu.display();
     }
